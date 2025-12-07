cebd7b4331b3abf48de41c41a162070a6c379ff7
Adrian <adrian.nenu@postgrad.manchester.ac.uk>
2025-12-07 10:19:47 +0000

    fix-generate-video

--- src/generate_video.py
+++ src/generate_video.py
@@ -6,8 +6,8 @@ import torch
 import gymnasium
 import imageio
 
-from gaussian_policy import GaussianPolicy
-from util import load_policy_from_checkpoint, make_offscreen_env, generate_video
+from mpo.gaussian_policy import GaussianPolicy
+from mpo.util import load_policy_from_checkpoint, make_offscreen_env, generate_video
 
 def main():
     parser = argparse.ArgumentParser(description="Generate video from MPO checkpoint")
