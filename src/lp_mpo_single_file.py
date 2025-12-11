03018a22caf440403babad2323f7c7d66b2b94cc
Adrian <adrian.nenu@postgrad.manchester.ac.uk>
2025-12-11 13:34:54 +0000

    helpers

--- src/lp_mpo_single_file.py
+++ src/lp_mpo_single_file.py
@@ -14,6 +14,8 @@ import reverb
 import pickle
 import wandb
 from dm_control import suite
+import tree
+import operator
 
 import acme
 from acme import specs
@@ -31,7 +33,7 @@ from acme import wrappers
 from acme.utils import paths
 from acme.utils import signals
 from acme.wrappers import mujoco as mujoco_wrappers
-
+from acme.utils import observers as observers_lib
 
 import tensorflow as tf
 import tensorflow_probability as tfp
@@ -51,6 +53,188 @@ _MAX_ACTOR_STEPS = flags.DEFINE_integer(
 _DOMAIN = flags.DEFINE_string("domain", "cartpole", "Control suite domain name.")
 _TASK = flags.DEFINE_string("task", "balance", "Control suite task name.")
 _MPO_FLOAT_EPSILON = 1e-8
+
+
+class EnvironmentLoop(core.Worker):
+    """A simple RL environment loop.
+
+    This takes `Environment` and `Actor` instances and coordinates their
+    interaction. Agent is updated if `should_update=True`. This can be used as:
+
+      loop = EnvironmentLoop(environment, actor)
+      loop.run(num_episodes)
+
+    A `Counter` instance can optionally be given in order to maintain counts
+    between different Acme components. If not given a local Counter will be
+    created to maintain counts between calls to the `run` method.
+
+    A `Logger` instance can also be passed in order to control the output of the
+    loop. If not given a platform-specific default logger will be used as defined
+    by utils.loggers.make_default_logger. A string `label` can be passed to easily
+    change the label associated with the default logger; this is ignored if a
+    `Logger` instance is given.
+
+    A list of 'Observer' instances can be specified to generate additional metrics
+    to be logged by the logger. They have access to the 'Environment' instance,
+    the current timestep datastruct and the current action.
+    """
+
+    def __init__(
+        self,
+        environment: dm_env.Environment,
+        actor: core.Actor,
+        counter: Optional[counting.Counter] = None,
+        logger: Optional[loggers.Logger] = None,
+        should_update: bool = True,
+        label: str = "environment_loop",
+        observers: Sequence[observers_lib.EnvLoopObserver] = (),
+    ):
+        # Internalize agent and environment.
+        self._environment = environment
+        self._actor = actor
+        self._counter = counter or counting.Counter()
+        self._logger = logger or loggers.make_default_logger(
+            label, steps_key=self._counter.get_steps_key()
+        )
+        self._should_update = should_update
+        self._observers = observers
+
+    def run_episode(self) -> loggers.LoggingData:
+        """Run one episode.
+
+        Each episode is a loop which interacts first with the environment to get an
+        observation and then give that observation to the agent in order to retrieve
+        an action.
+
+        Returns:
+          An instance of `loggers.LoggingData`.
+        """
+        # Reset any counts and start the environment.
+        episode_start_time = time.time()
+        select_action_durations: List[float] = []
+        env_step_durations: List[float] = []
+        episode_steps: int = 0
+
+        # For evaluation, this keeps track of the total undiscounted reward
+        # accumulated during the episode.
+        episode_return = tree.map_structure(
+            _generate_zeros_from_spec, self._environment.reward_spec()
+        )
+        env_reset_start = time.time()
+        timestep = self._environment.reset()
+        env_reset_duration = time.time() - env_reset_start
+        # Make the first observation.
+        self._actor.observe_first(timestep)
+        for observer in self._observers:
+            # Initialize the observer with the current state of the env after reset
+            # and the initial timestep.
+            observer.observe_first(self._environment, timestep)
+
+        # Run an episode.
+        while not timestep.last():
+            # Book-keeping.
+            episode_steps += 1
+
+            # Generate an action from the agent's policy.
+            select_action_start = time.time()
+            action = self._actor.select_action(timestep.observation)
+            select_action_durations.append(time.time() - select_action_start)
+
+            # Step the environment with the agent's selected action.
+            env_step_start = time.time()
+            timestep = self._environment.step(action)
+            env_step_durations.append(time.time() - env_step_start)
+
+            # Have the agent and observers observe the timestep.
+            self._actor.observe(action, next_timestep=timestep)
+            for observer in self._observers:
+                # One environment step was completed. Observe the current state of the
+                # environment, the current timestep and the action.
+                observer.observe(self._environment, timestep, action)
+
+            # Give the actor the opportunity to update itself.
+            if self._should_update:
+                self._actor.update()
+
+            # Equivalent to: episode_return += timestep.reward
+            # We capture the return value because if timestep.reward is a JAX
+            # DeviceArray, episode_return will not be mutated in-place. (In all other
+            # cases, the returned episode_return will be the same object as the
+            # argument episode_return.)
+            episode_return = tree.map_structure(
+                operator.iadd, episode_return, timestep.reward
+            )
+
+        # Record counts.
+        counts = self._counter.increment(episodes=1, steps=episode_steps)
+
+        # Collect the results and combine with counts.
+        steps_per_second = episode_steps / (time.time() - episode_start_time)
+        result = {
+            "episode_length": episode_steps,
+            "episode_return": episode_return,
+            "steps_per_second": steps_per_second,
+            "env_reset_duration_sec": env_reset_duration,
+            "select_action_duration_sec": np.mean(select_action_durations),
+            "env_step_duration_sec": np.mean(env_step_durations),
+        }
+        result.update(counts)
+        for observer in self._observers:
+            result.update(observer.get_metrics())
+        return result
+
+    def run(
+        self,
+        num_episodes: Optional[int] = None,
+        num_steps: Optional[int] = None,
+    ) -> int:
+        """Perform the run loop.
+
+        Run the environment loop either for `num_episodes` episodes or for at
+        least `num_steps` steps (the last episode is always run until completion,
+        so the total number of steps may be slightly more than `num_steps`).
+        At least one of these two arguments has to be None.
+
+        Upon termination of an episode a new episode will be started. If the number
+        of episodes and the number of steps are not given then this will interact
+        with the environment infinitely.
+
+        Args:
+          num_episodes: number of episodes to run the loop for.
+          num_steps: minimal number of steps to run the loop for.
+
+        Returns:
+          Actual number of steps the loop executed.
+
+        Raises:
+          ValueError: If both 'num_episodes' and 'num_steps' are not None.
+        """
+
+        if not (num_episodes is None or num_steps is None):
+            raise ValueError('Either "num_episodes" or "num_steps" should be None.')
+
+        def should_terminate(episode_count: int, step_count: int) -> bool:
+            return (num_episodes is not None and episode_count >= num_episodes) or (
+                num_steps is not None and step_count >= num_steps
+            )
+
+        episode_count: int = 0
+        step_count: int = 0
+        with signals.runtime_terminator():
+            while not should_terminate(episode_count, step_count):
+                episode_start = time.time()
+                result = self.run_episode()
+                result = {**result, **{"episode_duration": time.time() - episode_start}}
+                episode_count += 1
+                step_count += int(result["episode_length"])
+                # Log the given episode results.
+                self._logger.write(result)
+
+        return step_count
+
+
+def _generate_zeros_from_spec(spec: specs.Array) -> np.ndarray:
+    return np.zeros(spec.shape, spec.dtype)
 
 
 class FeedForwardActor(core.Actor):
@@ -514,7 +698,7 @@ class MPO:
         replay: reverb.Client,
         variable_source: acme.VariableSource,
         counter: counting.Counter,
-    ) -> acme.EnvironmentLoop:
+    ) -> EnvironmentLoop:
         """The actor process."""
 
         action_spec = self._environment_spec.actions
@@ -573,7 +757,7 @@ class MPO:
         )
 
         # Create the run loop and return it.
-        return acme.EnvironmentLoop(environment, actor, counter, logger)
+        return EnvironmentLoop(environment, actor, counter, logger)
 
     def evaluator(
         self,
@@ -630,8 +814,7 @@ class MPO:
             "evaluator", time_delta=self._log_every, steps_key="evaluator_steps"
         )
 
-        # Create the run loop and return it.
-        return acme.EnvironmentLoop(environment, evaluator, counter, logger)
+        return EnvironmentLoop(environment, evaluator, counter, logger)
 
     def build(self, name="mpo"):
         """Build the distributed agent topology."""
